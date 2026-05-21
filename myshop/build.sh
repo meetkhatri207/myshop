22960cd580b6acbd3ffa365c37f8d23136160d53
meetkhatri207 <meet@ocean-across.com>
2026-05-21 18:26:23 +0530

    Update build.sh to use pip and manage static files

--- myshop/build.sh
+++ myshop/build.sh
@@ -1,7 +1,12 @@
 #!/usr/bin/env bash
+# exit on error
+set -o errexit
 
+# 1. Install dependencies using requirements.txt
 pip install -r requirements.txt
 
-python manage.py collectstatic --noinput
+# 2. Collect Static Files (Crucial for production Django)
+python manage.py collectstatic --no-input
 
+# 3. Run database migrations
 python manage.py migrate
