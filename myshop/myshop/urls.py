eaf6cd4e473831c0e3561b58e871e93f4f81c427
meetkhatri207 <meet@ocean-across.com>
2026-05-22 12:05:41 +0530

    Apply full production settings for static and media harmony

--- myshop/myshop/urls.py
+++ myshop/myshop/urls.py
@@ -18,7 +18,6 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-from django.views.static import serve
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -34,9 +33,4 @@ urlpatterns = [
 ),
 ]
 
-if not settings.DEBUG:
-    urlpatterns += [
-        path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
-    ]
-else:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
